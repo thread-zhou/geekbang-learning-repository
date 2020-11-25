20e73dbad2f92db29853df26735b38d05afe5eef
zhoujian <thread_zhou@126.com>
2020-11-25 14:55:09 +0800

    update network-program (调整该模块结构)

---
+++ dig-into-the-java-virtual-machine/src/main/java/com/zhoujian/jvm/chapter_01/Foo.java
@@ -0,0 +1,21 @@
+package com.zhoujian.chapter_01;
+
+/**
+ * @ClassName: Foo
+ * @Description: Demo
+ * @author: zhoujian
+ * @date: 2020-11-25 13:30
+ * @version: 1.0
+ */
+public class Foo {
+
+    public static void main(String[] args) {
+        boolean flag = true;
+        if (flag) {
+            System.out.println("Hello, Java!");
+        }
+        if (flag == true) {
+            System.out.println("Hello, JVM!");
+        }
+    }
+}
